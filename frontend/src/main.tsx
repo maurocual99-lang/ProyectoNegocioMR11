bc6f8b288ed3017a26be325f8fec8e11ea42b221
Macarena Pereira <pereiramacarena394@gmail.com>
2026-09-14 09:19:49 -0300

    refcatorizacion

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import '@coreui/coreui/dist/css/coreui.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+/*
+ * IMPORTANTE:
+ * Los estilos globales de librerías se cargan PRIMERO.
+ * Después App carga los estilos propios del proyecto.
+ */
+import "@coreui/coreui/dist/css/coreui.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import App from "./App";
+
+
+ReactDOM
+  .createRoot(
+    document.getElementById(
+      "root"
+    ) as HTMLElement
+  )
+  .render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
